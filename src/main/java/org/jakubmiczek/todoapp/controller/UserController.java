97ae755be623334864328684c0b2b9f1feb61181
kubaMiczuu <kubamiczuu@gmail.com>
2026-07-07 20:59:01 +0200

    feat: add User Rest Controller with Post, Get, Put and Delete endpoints.

---
+++ src/main/java/org/jakubmiczek/todoapp/controller/UserController.java
@@ -0,0 +1,49 @@
+package org.jakubmiczek.todoapp.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.jakubmiczek.todoapp.controller.dto.UserRequest;
+import org.jakubmiczek.todoapp.controller.dto.UserResponse;
+import org.jakubmiczek.todoapp.controller.dto.UserUpdateRequest;
+import org.jakubmiczek.todoapp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> addUser(@Valid @RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateUser(@Valid @RequestBody UserUpdateRequest userUpdateRequest) {
+        userService.updateUser(userUpdateRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String username) {
+        userService.deleteUser(username);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> allUsers = userService.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(allUsers);
+    }
+
+}
